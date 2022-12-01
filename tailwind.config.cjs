5bce343ea9d882ab18310b217abc2a74202caf3c
Tsai-Chieh <d0177381@gmail.com>
2022-12-01 22:41:45 +0800

    :wrench: add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-50': '#2EDFDF',
+        'primary-100': '#61DED7',
+        'primary-150': '#95EED9',
+        'primary-200': '#00948B',
+        'primary-250': '#3A6B68',
+        'secondary-50': '#2D9CDB',
+        'gray-i50': '#BDBDBD',
+        'gray-i100': '#9FA0A0',
+        'gray-i150': '#828282',
+        'gray-i200': '#4F4F4F',
+        'gray-i250': '#333333',
+        'gray-t50': 'rgba(151, 151, 151, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
