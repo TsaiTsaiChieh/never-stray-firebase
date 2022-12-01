abf45ff54b5b84c3d98d3d1cbeeea059a3084d81
Tsai-Chieh <d0177381@gmail.com>
2022-12-01 20:31:33 +0800

    :wrench: update content setting, due to style in className not working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
