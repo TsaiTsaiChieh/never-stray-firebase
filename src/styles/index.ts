e0bbd449940c4001a3f2eb681f75fecab690a0a3
Tsai-Chieh <d0177381@gmail.com>
2022-12-03 00:44:03 +0800

    :tada: init Style for reuse

---
+++ src/styles/index.ts
@@ -0,0 +1,5 @@
+export const Styles = {
+  container: 'max-w-[576px] w-[80%] md:max-w-[820px] md:w-[85%] lg:max-w-[1140px] lg:w-[90%] m-auto relative',
+
+  flexCenter: 'flex justify-center items-center',
+} as const
