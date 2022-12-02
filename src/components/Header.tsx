8213e77e8265876c1997944b44488fd729c8ba3b
Tsai-Chieh <d0177381@gmail.com>
2022-12-03 00:52:22 +0800

    :recycle: import Style and update style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+import { Styles } from '../styles'
+
 export const Header = () => (
   <div className='py-[5px] border-b-[3px] border-primary-100'>
-    <div className='sm:max-w-[576px] sm:w-[80%] md:max-w-[820px] md:w-[85%] max-w-[1440px] w-[90%] m-auto flex  items-center justify-center'>
+    <div className={`${Styles.container} ${Styles.flexCenter} lg:justify-between`}>
       <img src='/logo.svg' alt='logo' />
-      <div className='flex'>
+      <div className='flex absolute right-1 lg:relative lg:right-auto'>
         <div className='w-[34px] h-[34px] border-[2px] border-primary-100 rounded-full' />
-        <span className='my-auto ml-1 text-gray-i200'>登入</span>
+        <span className='my-auto ml-2 text-gray-i200'>登入</span>
       </div>
     </div>
   </div>
