53f4b18de09ef4010815db114b19402a12d03cd8
Tsai-Chieh <d0177381@gmail.com>
2022-12-04 15:13:57 +0800

    :sparkles: add MD, LG, color for linaria

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,18 @@
-export const Styles = {
-  container: 'max-w-[576px] w-[80%] md:max-w-[820px] md:w-[85%] lg:max-w-[1140px] lg:w-[90%] m-auto relative',
+export const MD = '@media (min-width: 768px)'
+export const LG = '@media (min-width: 1024px)'
 
-  flexCenter: 'flex justify-center items-center',
-} as const
+export const colors = {
+  'primary-10': '#EBF8F7',
+  'primary-50': '#2EDFDF',
+  'primary-100': '#61DED7',
+  'primary-150': '#95EED9',
+  'primary-200': '#00948B',
+  'primary-250': '#3A6B68',
+  'secondary-50': '#2D9CDB',
+  'gray-i50': '#BDBDBD',
+  'gray-i100': '#9FA0A0',
+  'gray-i150': '#828282',
+  'gray-i200': '#4F4F4F',
+  'gray-i250': '#333333',
+  'gray-t50': 'rgba(151, 151, 151, 0.3)',
+}
