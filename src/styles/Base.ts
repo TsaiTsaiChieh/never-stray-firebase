1156042d1cc94fb02929402717030e0357bc3cee
Tsai-Chieh <d0177381@gmail.com>
2022-12-04 15:13:11 +0800

    :tada: init Base styled component

---
+++ src/styles/Base.ts
@@ -0,0 +1,23 @@
+import { styled } from '@linaria/react'
+
+import { LG, MD } from '.'
+
+export const Contain = styled.div`
+  max-width: 576px;
+  width: 80%;
+  margin: auto;
+  position: relative;
+  &.flex-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  ${MD} {
+    max-width: 820px;
+    width: 85%;
+  }
+  ${LG} {
+    max-width: 1140px;
+    width: 90%;
+  }
+`
