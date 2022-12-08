58f34768885984ff8bcf8e67d2bedf35155e2b20
Tsai-Chieh <d0177381@gmail.com>
2022-12-08 20:46:57 +0800

    :tada: init PetList component

--- src/pages/PetList/PetList.tsx
+++ src/pages/PetList/PetList.tsx
@@ -1,10 +1,19 @@
 import { Category, Header } from '../../components'
+import { useGetPetsQuery } from '../../services/api'
+import { useAppSelector } from '../../store/hook'
 
-const PetList = () => (
-  <>
-    <Header />
-    <Category />
-  </>
+const PetList = () => {
+  const { filter } = useAppSelector((state) => state.pet)
+  const { data } = useGetPetsQuery(filter)
+
+  return (
+    <>
+      <Header />
+      <Category />
+      {data
+        && data.map((ele) => <span key={ele.animal_id}>{ele.animal_id}</span>)}
+    </>
   )
+}
 
 export default PetList
