e6b17c21bace62c9a575c64cce4f5caf584b18f5
Tsai-Chieh <d0177381@gmail.com>
2022-12-08 20:47:18 +0800

    :sparkles: import RTK and persist

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,23 @@ import './i18n'
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
+import store from './store'
 
+const persistor = persistStore(store)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
