86e48bca9586827430017a0398aaab2e525b4a84
Tsai-Chieh <d0177381@gmail.com>
2022-12-11 23:35:43 +0800

    :boom: add useEffect to handle url param changing and update filter

--- src/pages/PetList/PetList.tsx
+++ src/pages/PetList/PetList.tsx
@@ -1,12 +1,31 @@
+import { useEffect } from 'react'
+
+import { useParams } from 'react-router-dom'
+
 import { Category, Header } from '../../components'
+import { PetKindEnum } from '../../constants/enum'
 import { useGetPetsQuery } from '../../services/api'
-import { useAppSelector } from '../../store/hook'
+import { useAppDispatch, useAppSelector } from '../../store/hook'
+import { setFilter } from '../../store/reducers/petSlice'
 import { Container, PetContainer } from '../../styles/pages/PetList'
 import Card from './Card'
 
 const PetList = () => {
   const { filter } = useAppSelector((state) => state.pet)
   const { data } = useGetPetsQuery(filter)
+  const dispatch = useAppDispatch()
+  const params = useParams()
+  useEffect(() => {
+    const { kind } = params
+    if (kind) {
+      dispatch(
+        setFilter({
+          ...filter,
+          kind: PetKindEnum[kind.replace('kind=', '') as PetKindUrlType],
+        }),
+      )
+    }
+  }, [params])
 
   return (
     <>
@@ -14,7 +33,10 @@ const PetList = () => {
       <Category />
       <Container>
         <PetContainer>
-          {data && data.map((ele:PetType) => <Card key={ele.animal_id} detail={ele} />)}
+          {data
+            && data.map((ele: PetType) => (
+              <Card key={ele.animal_id} detail={ele} />
+            ))}
         </PetContainer>
       </Container>
     </>
