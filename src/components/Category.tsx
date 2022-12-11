c0dd073ab1db5c9e18c33d50bb38a6a4e74b406f
Tsai-Chieh <d0177381@gmail.com>
2022-12-11 23:32:44 +0800

    :boom: add Link to button for changing URL params

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 
 import {
- faCat, faDog, faPaw, faFilter,
+  faCat,
+  faDog,
+  faPaw,
+  faFilter,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
 import i18n from 'i18next'
+import { Link } from 'react-router-dom'
 
+import { PetKindEnum } from '../constants/enum'
 import { FlexCenter } from '../styles/Base'
 import {
- ButtonWrap, CategoryName, Container, FilterIconWrap, FilterText, Wrapper,
+  ButtonWrap,
+  CategoryName,
+  Container,
+  FilterIconWrap,
+  FilterText,
+  Wrapper,
 } from '../styles/components/Category'
 
 const Category = () => {
@@ -19,28 +29,33 @@ const Category = () => {
     setActiveId(idx)
   }
   const icons = [faPaw, faCat, faDog]
-  const categoryItem: CategoryItemType[] = names.map((name, i) => ({ id: i, name, icon: icons[i] }))
- return (
-   <Wrapper>
-     <Container>
-       <FilterIconWrap>
-         <FontAwesomeIcon icon={faFilter} />
-         <FilterText>{i18n.t('buttons.filter')}</FilterText>
-       </FilterIconWrap>
-       <FlexCenter>
-         {categoryItem.map(({ id, name, icon }) => (
-           <ButtonWrap
-             key={id}
-             className={clsx({ active: activeId === id })}
-             onClick={() => onClick(id)}
-           >
-             <FontAwesomeIcon icon={icon} />
-             <CategoryName>{name}</CategoryName>
-           </ButtonWrap>
-         ))}
-       </FlexCenter>
-     </Container>
-   </Wrapper>
- )
-    }
+  const categoryItem: CategoryItemType[] = names.map((name, i) => ({
+    id: i,
+    name,
+    icon: icons[i],
+  }))
+  return (
+    <Wrapper>
+      <Container>
+        <FilterIconWrap>
+          <FontAwesomeIcon icon={faFilter} />
+          <FilterText>{i18n.t('buttons.filter')}</FilterText>
+        </FilterIconWrap>
+        <FlexCenter>
+          {categoryItem.map(({ id, name, icon }) => (
+            <Link key={id} to={`/kind=${Object.keys(PetKindEnum)[id]}`}>
+              <ButtonWrap
+                className={clsx({ active: activeId === id })}
+                onClick={() => onClick(id)}
+              >
+                <FontAwesomeIcon icon={icon} />
+                <CategoryName>{name}</CategoryName>
+              </ButtonWrap>
+            </Link>
+          ))}
+        </FlexCenter>
+      </Container>
+    </Wrapper>
+  )
+}
 export default Category
