8a8f543642b13cc0bd48560acc5128eeb27ade72
Tsai-Chieh <d0177381@gmail.com>
2022-12-11 14:21:28 +0800

    :tada: init Avatar component

---
+++ src/pages/PetList/Avatar.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+import { Paw, PetImg, PetImgWrap } from '../../styles/pages/PetList'
+import useProgressiveImage from '../../utils/useProgressiveImage'
+
+interface Props {
+  src: string
+}
+const Avatar = ({ src }: Props) => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const nullImgPath = '/images/pet-null.svg'
+  const loadingImgPath = '/images/pet-mask.svg'
+  const img = src === '' ? nullImgPath : useProgressiveImage(src)
+  useEffect(() => {
+    if (img) setLoading(false)
+  }, [img])
+
+ return (
+   <PetImgWrap>
+     <Paw />
+     <PetImg loading={loading} src={img || loadingImgPath} />
+   </PetImgWrap>
+ )
+}
+
+export default Avatar
