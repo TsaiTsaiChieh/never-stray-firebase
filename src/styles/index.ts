05b7d3deb5e2ac3ae1ff1a7db018e926e78fc9a5
Tsai-Chieh <d0177381@gmail.com>
2022-12-11 14:35:26 +0800

    :sparkles: update colors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,17 +3,21 @@ export const LG = '@media (min-width: 1024px)'
 
 export const colors = {
   'primary-10': '#EBF8F7',
+  'primary-20': '#EBFAF6',
+  'primary-30': '#E2F5F0',
   'primary-50': '#2EDFDF',
   'primary-100': '#61DED7',
   'primary-150': '#95EED9',
   'primary-200': '#00948B',
   'primary-250': '#3A6B68',
   'secondary-50': '#2D9CDB',
+  'blue-50': '#63B1E1',
+  'pink-50': '#FFA8A8',
   'gray-i50': '#BDBDBD',
   'gray-i100': '#9FA0A0',
   'gray-i150': '#828282',
   'gray-i200': '#4F4F4F',
   'gray-i250': '#333333',
-  'gray-t50': 'rgba(151, 151, 151, 0.3)',
+  'gray-t50': 'rgba(151, 151, 151, 0.2)',
   white: '#FFFFFF',
 }
