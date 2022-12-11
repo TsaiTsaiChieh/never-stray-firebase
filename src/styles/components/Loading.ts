a040028d7bdf1fa9baef3acb367876331fb32c4a
Tsai-Chieh <d0177381@gmail.com>
2022-12-12 01:13:51 +0800

    :tada: init Loading styled component

---
+++ src/styles/components/Loading.ts
@@ -0,0 +1,17 @@
+import { styled } from '@linaria/react'
+
+import { colors } from '../index'
+
+export const CatLoadingWrap = styled.div`
+  z-index: 1000;
+  position: fixed;
+  width: 100vw;
+  height: 100%;
+  background: ${colors['primary-t100']};
+  display: ${(props) => (props.visible ? 'flex' : 'none')};
+  align-items: center;
+  justify-content: center;
+  svg {
+    width: 250px !important;
+  }
+`
