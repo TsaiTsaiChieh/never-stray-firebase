f4baa9bc442889b66193e69b405176642e8e7c4b
Tsai-Chieh <d0177381@gmail.com>
2022-12-11 23:26:20 +0800

    :sparkles: add default router, ref from https://stackoverflow.com/questions/70437952/is-there-a-way-to-set-a-default-route-with-react-router-v6

--- src/routes/View.tsx
+++ src/routes/View.tsx
@@ -3,8 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import PetList from '../pages/PetList/PetList'
 
 const View = () => (
+
   <Routes>
-    <Route path='/' element={<PetList />} />
+    <Route path='/'>
+      <Route index element={<PetList />} />
+      <Route path='/:kind' element={<PetList />} />
+    </Route>
   </Routes>
   )
 
