c10ce20b2c6f86121dec65ef6219e8996335e383
Tsai-Chieh <d0177381@gmail.com>
2022-12-15 00:08:10 +0800

    :sparkles: set button as Link component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,33 +1,56 @@
 import {
- faAnglesRight, faAngleRight, faAngleLeft, faAnglesLeft,
+  faAnglesRight,
+  faAngleRight,
+  faAngleLeft,
+  faAnglesLeft,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
 import { Link, useLocation } from 'react-router-dom'
 
 import { useAppSelector } from '../store/hook/index'
-import { LeftWrap, PageWrapper, RightWrap } from '../styles/components/Pagination'
+import {
+  LeftWrap,
+  PageButton,
+  PageWrapper,
+  RightWrap,
+} from '../styles/components/Pagination'
 
 const Pagination = () => {
   const { filter } = useAppSelector((state) => state.pet)
-const { pathname } = useLocation()
+  const { pathname } = useLocation()
+  const { page } = filter
+  const disablePage = (max: number) => page < max
 
   return (
     <PageWrapper>
       <LeftWrap>
-        <Link to={`${pathname}?page=${filter.page - 10}`}>
+        <PageButton
+          as={Link}
+          className={clsx({ disabled: disablePage(10) })}
+          to={`${pathname}?page=${page - 10}`}
+        >
           <FontAwesomeIcon icon={faAnglesLeft} />
-        </Link>
-        <Link to={`${pathname}?page=${filter.page - 1}`}>
+        </PageButton>
+        <PageButton
+          as={Link}
+          className={clsx({ disabled: disablePage(1) })}
+          to={`${pathname}?page=${page - 1}`}
+        >
           <FontAwesomeIcon icon={faAngleLeft} />
-        </Link>
+        </PageButton>
       </LeftWrap>
+
       <RightWrap>
-        <Link to={`${pathname}?page=${filter.page + 1}`}>
+        <PageButton as={Link} to={`${pathname}?page=${page + 1}`}>
           <FontAwesomeIcon icon={faAngleRight} />
-        </Link>
-        <Link to={`${pathname}?page=${filter.page + 10}`}>
-          <FontAwesomeIcon icon={faAnglesRight} />
-        </Link>
+        </PageButton>
+        <PageButton as={Link} to={`${pathname}?page=${page + 10}`}>
+          <FontAwesomeIcon
+            icon={faAnglesRight}
+            to={`${pathname}?page=${page + 10}`}
+          />
+        </PageButton>
       </RightWrap>
     </PageWrapper>
   )
