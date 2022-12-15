df68f5c9f819a729d9f2fd57962782862bc246b8
Tsai-Chieh <d0177381@gmail.com>
2022-12-15 23:27:37 +0800

    :sparkles: import PageInput and set input value logic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,26 +1,46 @@
 import {
-  faAnglesRight,
-  faAngleRight,
-  faAngleLeft,
-  faAnglesLeft,
+ ChangeEvent, KeyboardEvent, useEffect, useState,
+} from 'react'
+
+import {
+  faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
-import { Link, useLocation } from 'react-router-dom'
+import {
+ Link, useLocation, useNavigate, useSearchParams,
+} from 'react-router-dom'
 
 import { useAppSelector } from '../store/hook/index'
 import {
-  LeftWrap,
-  PageButton,
-  PageWrapper,
-  RightWrap,
+  LeftWrap, PageButton, PageInput, PageWrapper, RightWrap,
 } from '../styles/components/Pagination'
 
 const Pagination = () => {
+  const navigate = useNavigate()
   const { filter } = useAppSelector((state) => state.pet)
-  const { pathname } = useLocation()
   const { page } = filter
+  const [pageValue, setPageValue] = useState(page.toString())
+  const location = useLocation()
+const { pathname } = location
+  const [searchParams] = useSearchParams()
+
   const disablePage = (max: number) => page < max
+  const changePageInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setPageValue(e.target.value)
+  }
+  const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (!/[0-9]/.test(e.key)) e.preventDefault()
+  }
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      navigate(`${pathname}?page=${parseInt(pageValue, 10)}`)
+    }
+  }
+  useEffect(() => {
+    const pageUrl = searchParams.get('page')
+    if (pageUrl) setPageValue(pageUrl)
+  }, [location.search])
 
   return (
     <PageWrapper>
@@ -40,7 +60,13 @@ const Pagination = () => {
           <FontAwesomeIcon icon={faAngleLeft} />
         </PageButton>
       </LeftWrap>
-
+      <PageInput
+        type='number'
+        value={pageValue}
+        onKeyPress={(e) => onKeyPress(e)}
+        onKeyDown={(e) => onKeyDown(e)}
+        onChange={(e) => changePageInput(e)}
+      />
       <RightWrap>
         <PageButton as={Link} to={`${pathname}?page=${page + 1}`}>
           <FontAwesomeIcon icon={faAngleRight} />
