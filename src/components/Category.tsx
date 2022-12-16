5fb89faac7b180566818eb835c67200d2d71459a
Tsai-Chieh <d0177381@gmail.com>
2022-12-17 01:15:33 +0800

    :boom: dispatch kind state, rather use react-router-dom's Link

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
 
 import {
-  faCat,
-  faDog,
-  faPaw,
-  faFilter,
+ faCat, faDog, faFilter, faPaw,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
 import i18n from 'i18next'
-import { Link } from 'react-router-dom'
 
 import { PetKindEnum } from '../constants/enum'
+import { useAppDispatch, useAppSelector } from '../store/hook'
+import { setFilter } from '../store/reducers/petSlice'
 import { FlexCenter } from '../styles/Base'
 import {
-  ButtonWrap,
-  CategoryName,
-  Container,
-  FilterIconWrap,
-  FilterText,
-  Wrapper,
+  ButtonWrap, CategoryName, Container, FilterIconWrap, FilterText, Wrapper,
 } from '../styles/components/Category'
 
 const Category = () => {
+  const dispatch = useAppDispatch()
   const [activeId, setActiveId] = useState(0)
+  const { filter } = useAppSelector((state) => state.pet)
   const names: string[] = i18n.t('buttons.categories', { returnObjects: true })
   const onClick = (idx: number) => {
     setActiveId(idx)
+    dispatch(
+      setFilter({
+        ...filter,
+        kind: Object.keys(PetKindEnum)[idx] as PetKindUrlType,
+        page: 1,
+      }),
+    )
   }
   const icons = [faPaw, faCat, faDog]
   const categoryItem: CategoryItemType[] = names.map((name, i) => ({
@@ -34,6 +36,7 @@ const Category = () => {
     name,
     icon: icons[i],
   }))
+
   return (
     <Wrapper>
       <Container>
@@ -43,15 +46,14 @@ const Category = () => {
         </FilterIconWrap>
         <FlexCenter>
           {categoryItem.map(({ id, name, icon }) => (
-            <Link key={id} to={`/kind=${Object.keys(PetKindEnum)[id]}`}>
-              <ButtonWrap
-                className={clsx({ active: activeId === id })}
-                onClick={() => onClick(id)}
-              >
-                <FontAwesomeIcon icon={icon} />
-                <CategoryName>{name}</CategoryName>
-              </ButtonWrap>
-            </Link>
+            <ButtonWrap
+              key={id}
+              className={clsx({ active: activeId === id })}
+              onClick={() => onClick(id)}
+            >
+              <FontAwesomeIcon icon={icon} />
+              <CategoryName>{name}</CategoryName>
+            </ButtonWrap>
           ))}
         </FlexCenter>
       </Container>
