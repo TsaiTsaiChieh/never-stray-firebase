fc301976690a92b5069a33e7421887ce10f77a7c
Tsai-Chieh <d0177381@gmail.com>
2022-12-18 23:01:11 +0800

    :bug: should active the correct category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -9,7 +9,11 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
 import i18n from 'i18next'
-import { createSearchParams, useNavigate } from 'react-router-dom'
+import {
+  createSearchParams,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom'
 
 import { PetKindEnum } from '../constants/enum'
 import { Paths } from '../constants/index'
@@ -26,22 +30,25 @@ import {
 
 const Category = () => {
   const navigate = useNavigate()
-  const [activeId, setActiveId] = useState(0)
   const { filter } = useAppSelector((state) => state.pet)
+  const [searchParams] = useSearchParams()
+  const [activeId, setActiveId] = useState<PetKindUrlType>(
+    searchParams.get('kind') as PetKindUrlType,
+  )
   const names: string[] = i18n.t('buttons.categories', { returnObjects: true })
-  const onClick = (idx: number) => {
-    setActiveId(idx)
+  const onClick = (i: number) => {
+    setActiveId(Object.keys(PetKindEnum)[i] as PetKindUrlType)
     navigate({
       pathname: Paths.home,
       search: createSearchParams({
-        kind: Object.keys(PetKindEnum)[idx] as PetKindUrlType,
+        kind: Object.keys(PetKindEnum)[i],
         page: filter.page.toString(),
       }).toString(),
     })
   }
   const icons = [faPaw, faCat, faDog]
   const categoryItem: CategoryItemType[] = names.map((name, i) => ({
-    id: i,
+    id: Object.keys(PetKindEnum)[i] as PetKindUrlType,
     name,
     icon: icons[i],
   }))
@@ -54,11 +61,11 @@ const Category = () => {
           <FilterText>{i18n.t('buttons.filter')}</FilterText>
         </FilterIconWrap>
         <FlexCenter>
-          {categoryItem.map(({ id, name, icon }) => (
+          {categoryItem.map(({ id, name, icon }, i) => (
             <ButtonWrap
               key={id}
               className={clsx({ active: activeId === id })}
-              onClick={() => onClick(id)}
+              onClick={() => onClick(i)}
             >
               <FontAwesomeIcon icon={icon} />
               <CategoryName>{name}</CategoryName>
