9d6393169be487951b4325b3f6233e15ffbfee24
Tsai-Chieh <d0177381@gmail.com>
2022-12-20 17:51:52 +0800

    :fire: rm rollupOptions setting

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(), linaria()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString()
-          }
-        },
-      },
-    },
+    sourcemap: true,
   },
 })
