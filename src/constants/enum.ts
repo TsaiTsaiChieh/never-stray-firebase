3ab9785b65dd3a992c0090f240f215df8bf50985
Tsai-Chieh <d0177381@gmail.com>
2022-12-22 23:21:36 +0800

    :label: add PetAgeEnum and tune A to W in PetKindEnum

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -1,5 +1,10 @@
 export const PetKindEnum: {[key in PetKindUrlType]: string| undefined} = {
-  A: undefined,
+  W: undefined,
   C: '貓',
   D: '狗',
+}
+export const PetAgeEnum: {[key in PetAgeUrlType]: string| undefined} = {
+  W: undefined,
+  A: 'adult',
+  C: 'child',
 }
