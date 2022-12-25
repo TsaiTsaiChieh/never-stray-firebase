f330cfa91eac3a5c32f93e05145c5c1c8bd034df
Tsai-Chieh <d0177381@gmail.com>
2022-12-25 23:34:25 +0800

    :sparkles: add go2home logic and format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,38 @@
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as i18n from 'i18next'
+import { useNavigate } from 'react-router-dom'
 
+import { Paths } from '../constants'
+import { useAppDispatch } from '../store/hook'
+import { resetState } from '../store/reducers/petSlice'
 import {
- AuthState, AvatarWrap, Container, Logo, Wrapper,
+  AuthState,
+  AvatarWrap,
+  Container,
+  Logo,
+  Wrapper,
 } from '../styles/components/Header'
 
-export const Header = () => (
-  <Wrapper>
-    <Container className='flex-center'>
-      <Logo />
-      <AvatarWrap>
-        <FontAwesomeIcon icon={faGoogle} />
-        <AuthState>{i18n.t('buttons.login')}</AuthState>
-      </AvatarWrap>
-    </Container>
-  </Wrapper>
-)
+export const Header = () => {
+  const dispatch = useAppDispatch()
+  const nav = useNavigate()
+  const go2home = () => {
+    nav({ pathname: Paths.home })
+    dispatch(resetState())
+  }
+
+  return (
+    <Wrapper>
+      <Container className='flex-center'>
+        <Logo onClick={go2home} />
+        <AvatarWrap>
+          <FontAwesomeIcon icon={faGoogle} />
+          <AuthState>{i18n.t('buttons.login')}</AuthState>
+        </AvatarWrap>
+      </Container>
+    </Wrapper>
+  )
+}
 
 export default Header
