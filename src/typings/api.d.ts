464620a18fa23d3cd379f5b2d3a862fb33efb480
Tsai-Chieh <d0177381@gmail.com>
2022-12-26 23:13:38 +0800

    :label: update GetPetReq

--- src/typings/api.d.ts
+++ src/typings/api.d.ts
@@ -3,11 +3,10 @@ type GetPetReq = {
   limit: number
   page: number
   kind: PetKindUrlType
-  animal_id?: number
+  id?: number
   age: PetAgeUrlType
   sex: PetSexUrlType
-  animal_sterilization?: string
   color?: string
   city?: number
-  animal_shelter_pkid?: number
+  shelter?: number
 }
