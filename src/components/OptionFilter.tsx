686fcf5fb00d9b51a2103d7f009a1f0ebb502066
Tsai-Chieh <d0177381@gmail.com>
2022-12-27 23:39:04 +0800

    :recycle: refactor AgeFilter and SexFilter 2 OptionFilter component

---
+++ src/components/OptionFilter.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+import { useSearchParams } from 'react-router-dom'
+
+import { PetAgeEnum, PetSexEnum } from '../constants/enum'
+import { useAppDispatch, useAppSelector } from '../store/hook'
+import { setFilter } from '../store/reducers/petSlice'
+import {
+  LabelName,
+  OptionBtn,
+  OptionBtnOuter,
+  OptionGroup,
+  OptionsFilterWrap,
+} from '../styles/components/LeftFilter'
+import { searchQuery } from '../utils/helper'
+
+interface Props {
+  fieldName: keyof Pick<GetPetReq, 'sex' | 'age'>
+  label: string
+  options: string[]
+}
+const OptionFilter = ({ fieldName, label, options }: Props) => {
+  const dispatch = useAppDispatch()
+  const optionEnum = fieldName === 'age' ? PetAgeEnum : PetSexEnum
+  const { filter } = useAppSelector((state) => state.pet)
+  const [searchParams] = useSearchParams()
+  let fieldIdx = 0
+  const urlParams = searchParams.get(fieldName)
+  if (urlParams !== null) {
+    fieldIdx = Object.keys(optionEnum).indexOf(urlParams) + 1
+  }
+  const [activeId, setActiveId] = useState<number>(fieldIdx)
+  const onClick = (i: number) => {
+    setActiveId(i)
+    const params = searchQuery(filter)
+    if (i !== 0) params[fieldName] = Object.keys(optionEnum)[i - 1]
+    else {
+      delete params[fieldName]
+      dispatch(
+        setFilter({
+          ...filter,
+          [fieldName]: undefined,
+        }),
+      )
+    }
+  }
+  return (
+    <OptionsFilterWrap>
+      <LabelName>{label}</LabelName>
+      <OptionGroup>
+        {options.map((option, i) => (
+          <OptionBtnOuter
+            key={option}
+            $active={i === activeId}
+            onClick={() => onClick(i)}
+          >
+            <OptionBtn>{option}</OptionBtn>
+          </OptionBtnOuter>
+        ))}
+      </OptionGroup>
+    </OptionsFilterWrap>
+  )
+}
+
+export default OptionFilter
