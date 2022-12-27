36effb6d87e12260f49f22b8f1ca996a26a36a2c
Tsai-Chieh <d0177381@gmail.com>
2022-12-27 23:43:54 +0800

    :sparkles: import OptionFilter

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Header } from './Header'
 export { default as Category } from './Category'
 export { default as CatLoading } from './CatLoading'
-export { default as LeftFilter } from './LeftFilter'
+export { default as OptionFilter } from './OptionFilter'
