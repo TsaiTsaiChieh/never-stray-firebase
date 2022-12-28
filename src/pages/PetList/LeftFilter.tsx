794a93aa9f8659b895510ad81c0cfbafca8e6874
Tsai-Chieh <d0177381@gmail.com>
2022-12-28 23:05:43 +0800

    :sparkles: ColorFilter and CityFilter 2 SelectorFilter and add species filter

--- src/pages/PetList/LeftFilter.tsx
+++ src/pages/PetList/LeftFilter.tsx
@@ -1,13 +1,14 @@
 import i18n from 'i18next'
 
 import { SelectorFilter } from '../../components'
-import CityFilter from '../../components/CityFilter'
-import ColorFilter from '../../components/filters/ColorFilter'
 import OptionFilter from '../../components/OptionFilter'
 import { FilterContainer } from '../../styles/pages/LeftFilter'
 
 const LeftFilter = () => {
   const Labels: string[] = i18n.t('labels.filters', { returnObjects: true })
+  const SpeciesOpts: string[] = i18n.t('filters.species', {
+    returnObjects: true,
+  })
   const AgeOpts: string[] = i18n.t('filters.ages', {
     returnObjects: true,
   })
@@ -17,23 +18,30 @@ const LeftFilter = () => {
   const ColorOpts: string[] = i18n.t('filters.colors', {
     returnObjects: true,
   })
+  const Placeholders: string[] = i18n.t('placeholders.selector', {
+    returnObjects: true,
+  })
   const CityOpts: LabelValueType[] = i18n.t('filters.cities', { returnObjects: true })
   const ShelterOpts: LabelValueType[] = i18n.t('filters.shelters', { returnObjects: true })
-  const ColorPlaceholder: string = i18n.t('placeholders.color')
-  const CityPlaceholder: string = i18n.t('placeholders.city')
-  const ShelterPlaceholder: string = i18n.t('placeholders.shelter')
 
   return (
     <FilterContainer>
       <OptionFilter fieldName='age' label={Labels[0]} options={AgeOpts} />
       <OptionFilter fieldName='sex' label={Labels[1]} options={SexOpts} />
-      <ColorFilter
-        label={Labels[2]}
+      <SelectorFilter fieldName='species' label={Labels[2]} options={SpeciesOpts.map((ele) => ({ label: ele, value: ele }))} placeholder={Placeholders[0]} />
+      <SelectorFilter
+        fieldName='color'
+        label={Labels[3]}
         options={ColorOpts.map((ele) => ({ label: ele, value: ele }))}
-        placeholder={ColorPlaceholder}
+        placeholder={Placeholders[1]}
       />
-      <CityFilter label={Labels[3]} options={CityOpts} placeholder={CityPlaceholder} />
-      <SelectorFilter fieldName='shelter' label={Labels[4]} options={ShelterOpts} placeholder={ShelterPlaceholder} />
+      <SelectorFilter
+        fieldName='city'
+        label={Labels[4]}
+        options={CityOpts}
+        placeholder={Placeholders[2]}
+      />
+      <SelectorFilter fieldName='shelter' label={Labels[5]} options={ShelterOpts} placeholder={Placeholders[3]} />
     </FilterContainer>
   )
 }
