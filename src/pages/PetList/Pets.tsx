eaa6e684ee3010971ca1392d837c2bb1370547ad
Tsai-Chieh <d0177381@gmail.com>
2023-01-04 22:20:20 +0800

    :tada: init Pets component

---
+++ src/pages/PetList/Pets.tsx
@@ -0,0 +1,30 @@
+import { CatLoading } from '../../components'
+import Pagination from '../../components/Pagination'
+import { useAppSelector } from '../../store/hook'
+import {
+  NotFound,
+  PetContainer,
+} from '../../styles/pages/PetList'
+import Card from './Card'
+
+interface Props {
+  data: PetType[]
+}
+const Pets = ({ data }: Props) => {
+  const { catLoading } = useAppSelector((state) => state.loading)
+
+  return (
+    <>
+      <PetContainer>
+        {catLoading ? <CatLoading /> : null}
+        {!catLoading
+          && data.length
+          && data.map((ele) => <Card key={ele.animal_id} detail={ele} />)}
+        {!catLoading && !data.length && <NotFound />}
+      </PetContainer>
+      <Pagination />
+    </>
+  )
+}
+
+export default Pets
