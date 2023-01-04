b1df851e4531b4678d79d9fd1a81f61afb30cd81
Tsai-Chieh <d0177381@gmail.com>
2023-01-04 22:42:48 +0800

    :bug: return null when the condition is false

--- src/pages/PetList/Pets.tsx
+++ src/pages/PetList/Pets.tsx
@@ -1,10 +1,7 @@
 import { CatLoading } from '../../components'
 import Pagination from '../../components/Pagination'
 import { useAppSelector } from '../../store/hook'
-import {
-  NotFound,
-  PetContainer,
-} from '../../styles/pages/PetList'
+import { NotFound, PetContainer } from '../../styles/pages/PetList'
 import Card from './Card'
 
 interface Props {
@@ -17,10 +14,10 @@ const Pets = ({ data }: Props) => {
     <>
       <PetContainer>
         {catLoading ? <CatLoading /> : null}
-        {!catLoading
-          && data.length
-          && data.map((ele) => <Card key={ele.animal_id} detail={ele} />)}
-        {!catLoading && !data.length && <NotFound />}
+        {!catLoading && data.length
+          ? data.map((ele) => <Card key={ele.animal_id} detail={ele} />)
+          : null}
+        {!catLoading && !data.length ? <NotFound /> : null}
       </PetContainer>
       <Pagination />
     </>
