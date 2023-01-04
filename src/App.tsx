771ab84a19ddc64385c7b405ab719ff0102d34fb
Tsai-Chieh <d0177381@gmail.com>
2023-01-04 22:32:28 +0800

    :fire: rm CatLoading importing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { CatLoading } from './components'
 import View from './routes/View'
 
 export const App = () => (
-  <>
-    <CatLoading />
-    <View />
-  </>
-
+  <View />
 )
 
 export default App
