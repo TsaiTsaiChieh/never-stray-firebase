1e95de1a4cf4c04b49cedde00aef3bd9a08ab5ca
Tsai-Chieh <d0177381@gmail.com>
2023-01-04 22:30:24 +0800

    :lipstick: update catLoading style

--- src/styles/components/Loading.ts
+++ src/styles/components/Loading.ts
@@ -1,16 +1,15 @@
 import { styled } from '@linaria/react'
 
-import { colors } from '../index'
-
 export const CatLoadingWrap = styled.div`
-  z-index: 1000;
-  position: fixed;
-  width: 100vw;
   height: 100%;
-  background: ${colors['gray-t100']};
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  position: absolute;
   display: ${(props) => (props.visible ? 'flex' : 'none')};
   align-items: center;
   justify-content: center;
+  z-index: 30;
   svg {
     width: 250px !important;
   }
