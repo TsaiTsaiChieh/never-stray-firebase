31ad92647ceba6eb74ab6721a9f2829e8ae552bf
Tsai-Chieh <d0177381@gmail.com>
2023-01-10 00:09:27 +0800

    :boom: change 2 forwardRef component for passing ref

--- src/pages/PetList/Card.tsx
+++ src/pages/PetList/Card.tsx
@@ -1,9 +1,17 @@
+import { forwardRef, Ref } from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import {
   AgeText,
- Bar,
- CardContainer, KindText, LearnMore, Name, OuterHoverWrap, SexText, SexWrap,
+  Bar,
+  CardContainer,
+  KindText,
+  LearnMore,
+  Name,
+  OuterHoverWrap,
+  SexText,
+  SexWrap,
 } from '../../styles/pages/PetList'
 import {
   mixAntiReplace,
@@ -16,30 +24,38 @@ import Avatar from './Avatar'
 interface Props {
   detail: PetType
 }
-const Card = ({ detail }: Props) => {
-  const {
- animal_colour, animal_Variety, animal_kind, animal_sex, animal_age,
-} = detail
-  const petName = `${animal_colour.replace('色', '')}${petKindConverter(animal_kind)}`
-  const sex = petSexConverter(animal_sex)
-  const age = petAgeConverter(animal_age)
-  return (
-    <OuterHoverWrap>
-      <CardContainer>
-        <Avatar src={detail.album_file} />
-        <Name>{petName}</Name>
-        <KindText>{mixAntiReplace(animal_Variety)}</KindText>
-        <LearnMore as='button'>
-          <SexWrap>
-            <FontAwesomeIcon icon={sex.icon} color={sex.color} />
-            <SexText>{sex.text}</SexText>
-          </SexWrap>
-          <Bar />
-          <AgeText>{age}</AgeText>
-        </LearnMore>
-      </CardContainer>
-    </OuterHoverWrap>
-  )
-}
+export const Card = forwardRef(
+  ({ detail }: Props, ref: Ref<HTMLDivElement>) => {
+    const {
+      animal_colour,
+      animal_Variety,
+      animal_kind,
+      animal_sex,
+      animal_age,
+    } = detail
+    const petName = `${animal_colour.replace('色', '')}${petKindConverter(
+      animal_kind,
+    )}`
+    const sex = petSexConverter(animal_sex)
+    const age = petAgeConverter(animal_age)
 
-export default Card
+    return (
+      <OuterHoverWrap ref={ref}>
+        <CardContainer>
+          <Avatar src={detail.album_file} />
+          <Name>{petName}</Name>
+          <KindText>{mixAntiReplace(animal_Variety)}</KindText>
+          <LearnMore as='button'>
+            <SexWrap>
+              <FontAwesomeIcon icon={sex.icon} color={sex.color} />
+              <SexText>{sex.text}</SexText>
+            </SexWrap>
+            <Bar />
+            <AgeText>{age}</AgeText>
+          </LearnMore>
+        </CardContainer>
+      </OuterHoverWrap>
+    )
+  },
+)
+Card.displayName = 'Card'
