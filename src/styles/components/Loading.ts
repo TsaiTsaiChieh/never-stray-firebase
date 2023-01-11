c01576b17f07134dcb121c43712061b1b209ab91
Tsai-Chieh <d0177381@gmail.com>
2023-01-11 15:41:10 +0800

    :sparkles: add MainLoading relatived styled-component

--- src/styles/components/Loading.ts
+++ src/styles/components/Loading.ts
@@ -1,6 +1,7 @@
 import { styled } from '@linaria/react'
 
 import { colors, MD } from '..'
+import { FlexCenter } from '../Base'
 
 export const CatLoadingWrap = styled.div`
   height: 100%;
@@ -79,4 +80,131 @@ export const Paw = styled.svg`
       opacity: 0;
     }
   }
+`
+export const MainLoadingContainer = styled(FlexCenter)<{ $visible: boolean }>`
+  display: ${(props) => (props.$visible ? 'flex' : 'none')};
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(180deg, #c4f1db 0%, #b9eff2 100%);
+  flex-direction: column;
+`
+export const Sparkles = styled.g`
+  & > path {
+    animation: sparklyBits 1000ms infinite;
+    position: absolute;
+  }
+  & > path:nth-child(1) {
+    animation-delay: 0ms;
+  }
+  & > path:nth-child(2) {
+    animation-delay: 35ms;
+  }
+  & > path:nth-child(3) {
+    animation-delay: 70ms;
+  }
+  & > path:nth-child(4) {
+    animation-delay: 105ms;
+  }
+  & > path:nth-child(5) {
+    animation-delay: 140ms;
+  }
+  & > path:nth-child(6) {
+    animation-delay: 175ms;
+  }
+  & > path:nth-child(7) {
+    animation-delay: 210ms;
+  }
+  & > path:nth-child(8) {
+    animation-delay: 245ms;
+  }
+  & > path:nth-child(9) {
+    animation-delay: 280ms;
+  }
+  & > path:nth-child(10) {
+    animation-delay: 315ms;
+  }
+  & > path:nth-child(11) {
+    animation-delay: 350ms;
+  }
+  & > path:nth-child(12) {
+    animation-delay: 385ms;
+  }
+  & > path:nth-child(13) {
+    animation-delay: 420ms;
+  }
+  & > path:nth-child(14) {
+    animation-delay: 455ms;
+  }
+  & > path:nth-child(15) {
+    animation-delay: 490ms;
+  }
+  & > path:nth-child(16) {
+    animation-delay: 525ms;
+  }
+  & > path:nth-child(17) {
+    animation-delay: 560ms;
+  }
+  & > path:nth-child(18) {
+    animation-delay: 595ms;
+  }
+  & > path:nth-child(19) {
+    animation-delay: 630ms;
+  }
+  & > path:nth-child(20) {
+    animation-delay: 665ms;
+  }
+  & > path:nth-child(21) {
+    animation-delay: 700ms;
+  }
+  & > path:nth-child(22) {
+    animation-delay: 735ms;
+  }
+  & > path:nth-child(23) {
+    animation-delay: 770ms;
+  }
+  & > path:nth-child(24) {
+    animation-delay: 805ms;
+  }
+  & > path:nth-child(25) {
+    animation-delay: 840ms;
+  }
+  & > path:nth-child(26) {
+    animation-delay: 875ms;
+  }
+  & > path:nth-child(27) {
+    animation-delay: 910ms;
+  }
+  & > path:nth-child(28) {
+    animation-delay: 945ms;
+  }
+  & > path:nth-child(29) {
+    animation-delay: 980ms;
+  }
+  @keyframes sparklyBits {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`
+export const Logo = styled.img`
+  position: absolute;
+  top: 50%;
+  bottom: 50%;
+  margin: auto;
+  margin-bottom: -20px;
+  width: 80px;
+  height: 80px;
+  content: url("/images/logo.svg");
+`
+export const LoadingText = styled.span`
+  margin-top: 15px;
+  margin-left: 10px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  letter-spacing: 0.08rem;
 `
