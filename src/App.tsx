34b6bc6a4cdd395fa78ae43a4e901a9bfe3c73f6
Tsai-Chieh <d0177381@gmail.com>
2023-01-11 15:43:45 +0800

    :sparkles: add MainLoading and timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import View from './routes/View'
+import { useEffect } from 'react'
 
-export const App = () => (
-  <View />
-)
+import { MainLoading } from './components'
+import View from './routes/View'
+import { useAppDispatch, useAppSelector } from './store/hook'
+import { setMainLoading } from './store/reducers/loadingSlice'
+
+export const App = () => {
+  const dispatch = useAppDispatch()
+  const { mainLoading } = useAppSelector((state) => state.loading)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(setMainLoading(false))
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [dispatch, mainLoading])
+
+  return mainLoading ? <MainLoading /> : <View />
+}
 
 export default App
