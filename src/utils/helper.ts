18628ceb72b4a96f9cd4d243c6ccc35f721866ff
Tsai-Chieh <d0177381@gmail.com>
2023-01-11 21:27:33 +0800

    :sparkles: tune petSexConverter returning

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,6 @@
 import { faMars, faQuestion, faVenus } from '@fortawesome/free-solid-svg-icons'
 
-import { colors } from '../styles'
+import { filters } from '../styles'
 
 const unknown = '未知'
 export const petKindConverter = (kind: string): string => {
@@ -23,14 +23,14 @@ export const petSexIconConverter = (sex: string) => {
       return faQuestion
   }
 }
-export const petSexConverter = (sex: string) => {
+export const petSexConverter = (sex: string): SexIconType => {
   switch (sex) {
     case 'M':
-      return { icon: faMars, text: '弟弟', color: colors['blue-50'] }
+      return { iconPath: '/images/mars.svg', name: '弟弟', color: filters['blue-50'] }
     case 'F':
-      return { icon: faVenus, text: '妹妹', color: colors['pink-50'] }
+      return { iconPath: '/images/venus.svg', name: '妹妹', color: filters['pink-50'] }
     default:
-      return { icon: faQuestion, text: unknown, color: colors['gray-i50'] }
+      return { iconPath: '/images/question.svg', name: unknown, color: filters['gray-i50'] }
   }
 }
 export const petAgeConverter = (age: string): string => {
