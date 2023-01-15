8b970abdbdcddcc082bab652b1e4f7ea412e5e68
Tsai-Chieh <d0177381@gmail.com>
2023-01-15 22:21:01 +0800

    :tada: ini crud and add createData func

---
+++ src/services/crud.ts
@@ -0,0 +1,26 @@
+import { setDoc, doc } from 'firebase/firestore'
+
+import { db } from './firebase'
+
+/**
+ * Add data
+ *
+ * @param  {CollectionName} col collection name
+ * @param  {string} id custom document ID
+ * @param  {any} data save data (single)
+ * @return {Promise<string>} custom document ID or error
+ */
+export const createData = async (
+  col: CollectionName,
+  id: string,
+  data: any,
+): Promise<string> => {
+  try {
+    const docRef = doc(db, col, id)
+    await setDoc(docRef, data, { merge: true })
+    console.info('Document written with ID', id)
+    return Promise.resolve(id)
+  } catch (error: any) {
+    return Promise.reject(error)
+  }
+}
