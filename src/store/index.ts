24545644b849843a2e9194c517c100827f849724
Tsai-Chieh <d0177381@gmail.com>
2023-01-18 16:47:38 +0800

    :sparkles: import auth slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,13 +10,13 @@ import {
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
-import { api } from '../services/api'
 import rootReducer from './rootReducer'
+import { api } from '../services/api'
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: [''],
+  whitelist: ['auth'],
   blacklist: ['pet', 'loading', 'ui'],
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer)
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,15 +1,17 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
-import { api } from '../services/api'
+import authReducer from './reducers/authSlice'
 import loadingReducer from './reducers/loadingSlice'
 import petReducer from './reducers/petSlice'
 import uiReducer from './reducers/uiSlice'
+import { api } from '../services/api'
 
 const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
   pet: petReducer,
   loading: loadingReducer,
   ui: uiReducer,
+  auth: authReducer,
 })
 
 export default rootReducer
