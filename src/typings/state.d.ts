86937eaa7c8616831e9017bd14ea36870e54a626
Tsai-Chieh <d0177381@gmail.com>
2023-01-22 17:13:55 +0800

    :label: add modalLoading in LoadingState

--- src/typings/state.d.ts
+++ src/typings/state.d.ts
@@ -1,9 +1,15 @@
 type PetState = {
   filter: GetPetReq
 }
+type ModalLoading = {
+  visible: boolean
+  title?: string
+  content?: string
+}
 type LoadingState = {
   mainLoading: boolean
   pageLoading: boolean
+  modalLoading: ModalLoading
 }
 type UiState = {
   filterVisible: boolean
