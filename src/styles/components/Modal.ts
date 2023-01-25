344d6b8613e9aa536fbc02875ae1deed0ccbd499
Tsai-Chieh <d0177381@gmail.com>
2023-01-25 22:30:36 +0800

    :lipstick: add confirm modal relatived styled component

--- src/styles/components/Modal.ts
+++ src/styles/components/Modal.ts
@@ -1,7 +1,7 @@
 import { styled } from '@linaria/react'
 
 import {
- alpha, colors, MD, shadow,
+ alpha, colors, filters, MD, shadow,
 } from '..'
 import { FlexCenter } from '../Base'
 
@@ -20,14 +20,14 @@ export const Frame = styled(FlexCenter)`
   flex-direction: column;
   position: absolute;
   margin: auto;
-  padding: 40px 120px;
+  padding: 40px 50px;
   border: 5px solid ${colors.secondary};
   border-radius: 5px;
   box-shadow: ${shadow.modal};
   background: ${colors.white};
   text-align: center;
   ${MD} {
-    padding: 40px 140px;
+    padding: 40px 100px;
   }
 `
 export const Loader = styled.div`
@@ -54,10 +54,53 @@ export const Title = styled.h2`
   font-weight: bold;
   font-size: 18px;
   letter-spacing: 0.04rem;
+  line-height: 1.4;
 `
 export const Content = styled.p`
   font-size: 15px;
   color: ${colors.gray3};
   letter-spacing: 0.01rem;
   margin-top: 8px;
+  line-height: 1.4;
+`
+export const WarningIcon = styled.img`
+  content: url("/images/warning.svg");
+  z-index: 10;
+  width: 70px;
+  margin-bottom: 20px;
+`
+export const BtnWrap = styled(FlexCenter)`
+  margin-top: 25px;
+`
+
+export const BtnOuter = styled(FlexCenter)`
+  width: 114px;
+  height: 50px;
+  border-radius: 5px;
+  &:hover {
+    background-color: ${alpha.primary};
+  }
+`
+export const ConfirmBtn = styled.button`
+  width: 110px;
+  font-size: 14px;
+  height: 46px;
+  letter-spacing: 0.05rem;
+  padding: 12px 20px;
+  color: ${colors.gray1};
+  border: 1px solid ${colors.primary};
+  border-radius: 5px;
+  background: ${colors.white};
+`
+export const XMark = styled.img`
+  width: 25px;
+  position: absolute;
+  top: 5px;
+  right: 13px;
+  content: url("/images/xmark.svg");
+  cursor: pointer;
+  filter: ${filters.gray2};
+  &:hover {
+    filter: ${filters.primary};
+  }
 `
