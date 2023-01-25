d94f0a9ecd73f5b21a76f3350ce5064023430477
Tsai-Chieh <d0177381@gmail.com>
2023-01-25 22:29:49 +0800

    :sparkles: add setOverLimitVisible reducer

--- src/store/reducers/uiSlice.ts
+++ src/store/reducers/uiSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
 const initialState: UiState = {
   filterVisible: false,
+  overLimitVisible: false,
 }
 
 export const uiSlice = createSlice({
@@ -11,9 +13,12 @@ export const uiSlice = createSlice({
     toggleFilterVisible: (state) => {
       state.filterVisible = !state.filterVisible
     },
+    setOverLimitVisible: (state, { payload }: PayloadAction<boolean>) => {
+      state.overLimitVisible = payload
+    },
   },
 })
 
-export const { toggleFilterVisible } = uiSlice.actions
+export const { toggleFilterVisible, setOverLimitVisible } = uiSlice.actions
 
 export default uiSlice.reducer
