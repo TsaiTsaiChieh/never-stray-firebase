581a18f75aeb90e6e7d1b54a28c39a9e1d3ae15e
Tsai-Chieh <d0177381@gmail.com>
2023-01-25 22:39:10 +0800

    :boom: update authHandle func and update user data when login and logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ import * as i18n from 'i18next'
 import { useNavigate } from 'react-router-dom'
 
 import { Paths } from '../constants'
-import { createData, readDoc } from '../services/crud'
+import { createData, readDoc, updateData } from '../services/crud'
 import { googleLogin } from '../services/users'
 import { useAppDispatch, useAppSelector } from '../store/hook'
-import { login, logout } from '../store/reducers/authSlice'
+import {
+ login, logout, setAuthLoading, setIsLike,
+} from '../store/reducers/authSlice'
 import { resetFilter } from '../store/reducers/petSlice'
 import {
   AuthState,
@@ -28,11 +30,13 @@ export const Header = () => {
   const go2home = () => {
     nav({ pathname: Paths.home })
     dispatch(resetFilter())
+    dispatch(setIsLike(false))
   }
   const { isAuth, userData } = useAppSelector((state) => state.auth)
   const authHandle = async () => {
     if (!isAuth) {
       const user = await googleLogin()
+      dispatch(setAuthLoading({ type: 'login', visible: true }))
       const { name, email, photo } = user
       const docSnap: UserInfoType = await readDoc('users', email)
       const saved: any = {
@@ -41,18 +45,40 @@ export const Header = () => {
         photo: photo !== null ? photo : '',
       }
       if (!docSnap) {
-        saved.like_limit = 18
+        saved.like_limit = import.meta.env.VITE_PET_LIMIT
+        saved.like_ids = []
         saved.create_time = new Date()
       }
       await createData('users', email, saved)
+      // delete non-serializable data
       delete saved.create_time
+      delete saved.update_time
       dispatch(
-        login(docSnap ? { ...saved, like_limit: docSnap.like_limit } : saved),
+        login(
+          docSnap
+            ? {
+                ...saved,
+                like_limit: docSnap.like_limit,
+                like_ids: docSnap.like_ids,
+              }
+            : saved,
+        ),
       )
-    } else if (isAuth) {
+      dispatch(setAuthLoading({ type: 'login', visible: false }))
+    } else if (isAuth && userData) {
+      dispatch(setAuthLoading({ type: 'logout', visible: true }))
+      await updateData('users', userData.email, {
+        like_ids: userData.like_ids,
+        like_limit:
+          userData.like_limit > import.meta.env.VITE_PET_LIMIT
+            ? import.meta.env.VITE_PET_LIMIT
+            : userData.like_limit,
+      })
       dispatch(logout())
+      dispatch(setAuthLoading({ type: 'logout', visible: false }))
     }
   }
+
   return (
     <Wrapper>
       <Container className='flex-center'>
