5c0d6dcc90692b01895103afa76c9c88a801d3d8
Tsai-Chieh <d0177381@gmail.com>
2023-01-27 00:54:37 +0800

    :recycle: make authHandle func more easy-understand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,10 @@ import { createData, readDoc, updateData } from '../services/crud'
 import { googleLogin } from '../services/users'
 import { useAppDispatch, useAppSelector } from '../store/hook'
 import {
- login, logout, setAuthLoading, setIsLike,
+  login,
+  logout,
+  setAuthLoading,
+  setIsLike,
 } from '../store/reducers/authSlice'
 import { resetFilter } from '../store/reducers/petSlice'
 import {
@@ -38,41 +41,35 @@ export const Header = () => {
       const user = await googleLogin()
       dispatch(setAuthLoading({ type: 'login', visible: true }))
       const { name, email, photo } = user
-      const docSnap: UserInfoType = await readDoc('users', email)
-      const saved: any = {
+      const docSnap: UserFromFirebase | undefined = await readDoc(
+        'users',
+        email,
+      )
+      const loginInfo: Omit<UserInfoType, 'like_pets'> = {
         email,
         name: name !== null ? name : '',
         photo: photo !== null ? photo : '',
       }
       if (!docSnap) {
-        saved.like_limit = import.meta.env.VITE_PET_LIMIT
-        saved.like_ids = []
-        saved.create_time = new Date()
+        const saved: UserInfoType = { ...loginInfo, like_pets: [] }
+        await createData('users', email, {
+          ...saved,
+          create_time: new Date(),
+        })
+        dispatch(login(saved))
+      } else if (docSnap) {
+        dispatch(
+          login({
+            ...loginInfo,
+            like_pets: docSnap.like_pets,
+          }),
+        )
       }
-      await createData('users', email, saved)
-      // delete non-serializable data
-      delete saved.create_time
-      delete saved.update_time
-      dispatch(
-        login(
-          docSnap
-            ? {
-                ...saved,
-                like_limit: docSnap.like_limit,
-                like_ids: docSnap.like_ids,
-              }
-            : saved,
-        ),
-      )
       dispatch(setAuthLoading({ type: 'login', visible: false }))
     } else if (isAuth && userData) {
       dispatch(setAuthLoading({ type: 'logout', visible: true }))
       await updateData('users', userData.email, {
-        like_ids: userData.like_ids,
-        like_limit:
-          userData.like_limit > import.meta.env.VITE_PET_LIMIT
-            ? import.meta.env.VITE_PET_LIMIT
-            : userData.like_limit,
+        like_pets: userData.like_pets,
       })
       dispatch(logout())
       dispatch(setAuthLoading({ type: 'logout', visible: false }))
