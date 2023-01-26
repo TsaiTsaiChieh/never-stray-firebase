f347743af8daf5548e3af1c1d4f5452cf7ae6e54
Tsai-Chieh <d0177381@gmail.com>
2023-01-27 00:52:43 +0800

    :sparkles: load limit from VITE_LIKE_LIMIT

--- src/components/Modals/OverLimit.tsx
+++ src/components/Modals/OverLimit.tsx
@@ -7,7 +7,7 @@ import { useAppSelector } from '../../store/hook'
 const OverLimit = () => {
   const { overLimitVisible } = useAppSelector((state) => state.ui)
   return (
-    <ConfirmModal visible={overLimitVisible} title={i18n.t('titles.over_limit')!} content={<Trans i18nKey='content.over_limit' values={{ limit: import.meta.env.VITE_PET_LIMIT }} />} />
+    <ConfirmModal visible={overLimitVisible} title={i18n.t('titles.over_limit')!} content={<Trans i18nKey='content.over_limit' values={{ limit: import.meta.env.VITE_LIKE_LIMIT }} />} />
   )
 }
 
