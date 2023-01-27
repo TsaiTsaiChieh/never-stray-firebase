c42f4f4c777c4685cb96418a46c7fd0964187271
Tsai-Chieh <d0177381@gmail.com>
2023-01-27 23:28:28 +0800

    :sparkles: content prop add string type, add confirmAction prop and rm dispatch action

--- src/components/Modals/ConfirmModal.tsx
+++ src/components/Modals/ConfirmModal.tsx
@@ -1,7 +1,5 @@
 import * as i18n from 'i18next'
 
-import { useAppDispatch } from '../../store/hook'
-import { setOverLimitVisible } from '../../store/reducers/uiSlice'
 import {
   BtnOuter,
   BtnWrap,
@@ -13,28 +11,25 @@ import {
 interface Props {
   visible: boolean
   title?: string
-  content?: React.ReactElement
+  content?: React.ReactElement | string
+  confirmAction: ()=> void
 }
-const ConfirmModal = ({ visible, title, content }: Props) => {
-  const dispatch = useAppDispatch()
-  const closeModal = () => {
-dispatch(setOverLimitVisible(false))
-  }
-  return (
-    <Mask $visible={visible}>
-      <Frame>
-        <XMark onClick={closeModal} />
-        <WarningIcon alt='warning' />
-        {title ? <Title>{title}</Title> : null}
-        {content ? <Content>{content}</Content> : null}
-        <BtnWrap>
-          <BtnOuter>
-            <ConfirmBtn onClick={closeModal}>{i18n.t('buttons.understand')}</ConfirmBtn>
-          </BtnOuter>
-        </BtnWrap>
-      </Frame>
-    </Mask>
+const ConfirmModal = ({
+ visible, title, content, confirmAction,
+}: Props) => (
+  <Mask $visible={visible}>
+    <Frame>
+      <XMark onClick={confirmAction} />
+      <WarningIcon alt='warning' />
+      {title ? <Title>{title}</Title> : null}
+      {content ? <Content>{content}</Content> : null}
+      <BtnWrap>
+        <BtnOuter>
+          <ConfirmBtn onClick={confirmAction}>{i18n.t('buttons.understand')}</ConfirmBtn>
+        </BtnOuter>
+      </BtnWrap>
+    </Frame>
+  </Mask>
   )
-}
 
 export default ConfirmModal
