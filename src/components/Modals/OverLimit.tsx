ddd9a5c7ab92e92149030c8c6302d4ada276ffb9
Tsai-Chieh <d0177381@gmail.com>
2023-01-27 23:33:40 +0800

    :sparkles: pass confirmAction

--- src/components/Modals/OverLimit.tsx
+++ src/components/Modals/OverLimit.tsx
@@ -2,12 +2,18 @@ import * as i18n from 'i18next'
 import { Trans } from 'react-i18next'
 
 import ConfirmModal from './ConfirmModal'
-import { useAppSelector } from '../../store/hook'
+import { useAppDispatch, useAppSelector } from '../../store/hook'
+import { setOverLimitVisible } from '../../store/reducers/uiSlice'
 
 const OverLimit = () => {
+  const dispatch = useAppDispatch()
   const { overLimitVisible } = useAppSelector((state) => state.ui)
+  const confirmAction = () => {
+    dispatch(setOverLimitVisible(false))
+  }
+
   return (
-    <ConfirmModal visible={overLimitVisible} title={i18n.t('titles.over_limit')!} content={<Trans i18nKey='content.over_limit' values={{ limit: import.meta.env.VITE_LIKE_LIMIT }} />} />
+    <ConfirmModal visible={overLimitVisible} title={i18n.t('titles.over_limit')!} content={<Trans i18nKey='content.over_limit' values={{ limit: import.meta.env.VITE_LIKE_LIMIT }} />} confirmAction={confirmAction} />
   )
 }
 
