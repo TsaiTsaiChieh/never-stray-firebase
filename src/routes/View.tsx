e8e8dab5b81dee9d3d4f30d8ed5c8efe4d6c059d
Tsai-Chieh <d0177381@gmail.com>
2023-01-28 19:47:57 +0800

    :sparkles: import Pet page

--- src/routes/View.tsx
+++ src/routes/View.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { Paths } from '../constants'
+import Pet from '../pages/Pet'
 import PetList from '../pages/PetList'
 
 const View = () => (
   <Routes>
     <Route path='*' element={<Navigate to={Paths.home} replace />} />
     <Route path={Paths.home} element={<PetList />} />
+    <Route path={`${Paths.pet}/:id`} element={<Pet />} />
   </Routes>
   )
 
