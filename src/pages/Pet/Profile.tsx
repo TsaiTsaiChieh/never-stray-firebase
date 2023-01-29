8453ece732dc740aa84fbff246907af7e5555377
Tsai-Chieh <d0177381@gmail.com>
2023-01-29 21:47:18 +0800

    :tada: init Profile component

---
+++ src/pages/Pet/Profile.tsx
@@ -0,0 +1,71 @@
+import * as i18n from 'i18next'
+
+import InfoBoxes from './InfoBoxes'
+import {
+  Avatar,
+  Container,
+  Label,
+  LabelAndValue,
+  ShelterName,
+  SummaryWrap,
+  Value,
+  UpdateTime,
+  IntroWrap,
+  AvatarWrap,
+  IntroLabel,
+  IntroValue,
+  DetailWrap,
+} from '../../styles/pages/Pet'
+import { dateFormatter } from '../../utils/helper'
+
+interface Props {
+  profile: PetType
+}
+const Profile = ({ profile }: Props) => {
+  const {
+    album_file,
+    animal_place,
+    animal_update,
+    animal_subid,
+    animal_colour,
+    animal_Variety,
+    animal_remark,
+  } = profile
+  console.log(profile)
+
+  return (
+    <Container>
+      <AvatarWrap>
+        <Avatar $content={album_file} />
+      </AvatarWrap>
+      <DetailWrap>
+        <SummaryWrap>
+          <ShelterName>{animal_place}</ShelterName>
+          <LabelAndValue>
+            <Label>{i18n.t('labels.update_time')}</Label>
+            <UpdateTime>{dateFormatter(animal_update)}</UpdateTime>
+          </LabelAndValue>
+          <LabelAndValue>
+            <Label>{i18n.t('labels.uid')}</Label>
+            <Value>{animal_subid}</Value>
+          </LabelAndValue>
+          <LabelAndValue>
+            <Label>{i18n.t('labels.color')}</Label>
+            <Value>{animal_colour}</Value>
+          </LabelAndValue>
+          <LabelAndValue>
+            <Label>{i18n.t('labels.species')}</Label>
+            <Value>{animal_Variety}</Value>
+          </LabelAndValue>
+        </SummaryWrap>
+        <InfoBoxes profile={profile} />
+        <IntroWrap>
+          <IntroLabel>{i18n.t('labels.introduction')}</IntroLabel>
+          <IntroValue>{animal_remark === '' ? i18n.t('placeholders.empty_intro') : animal_remark}</IntroValue>
+        </IntroWrap>
+      </DetailWrap>
+    </Container>
+  )
+}
+
+export default Profile
