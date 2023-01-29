46f021952864243b375450b90afa97fdd3b045f4
Tsai-Chieh <d0177381@gmail.com>
2023-01-29 21:56:10 +0800

    :sparkles: load pet detail from state or call API

--- src/pages/Pet/index.tsx
+++ src/pages/Pet/index.tsx
@@ -1,10 +1,34 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+import { useLocation, useParams } from 'react-router-dom'
+
+import Profile from './Profile'
+import { Footer, Header } from '../../components'
+import { useGetPetMutation } from '../../services/api'
 
 const Pet = () => {
+  const [profile, setProfile] = useState<PetType | undefined>(undefined)
+  const location = useLocation()
   const { id } = useParams()
+  const [getPet] = useGetPetMutation()
+
+  useEffect(() => {
+    const getPetDetail = async () => {
+      const result = await getPet({ id: id! }).unwrap()
+      setProfile(result[0])
+    }
+    if (location.state !== null) setProfile(location.state.detail)
+    else {
+      getPetDetail()
+    }
+  }, [location.state, id, getPet])
 
   return (
-    <div>{id}</div>
+    <>
+      <Header />
+      {profile ? <Profile profile={profile} /> : null}
+      <Footer />
+    </>
   )
 }
 
