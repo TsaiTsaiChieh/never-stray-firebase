92e251501e12830d777d19a3cc4afc85cc2f94f1
Tsai-Chieh <d0177381@gmail.com>
2023-01-29 21:47:42 +0800

    :tada: init style components for pet page

---
+++ src/styles/pages/Pet.ts
@@ -0,0 +1,94 @@
+import { styled } from '@linaria/react'
+
+import { colors, MD } from '..'
+import { Contain } from '../Base'
+
+export const Container = styled(Contain)`
+  margin-top: 20px;
+  ${MD} {
+    margin-top: 30px;
+    display: flex;
+    gap: 30px;
+  }
+`
+
+export const AvatarWrap = styled.div`
+  width: 100%;
+  ${MD} {
+    width: 50%;
+  }
+`
+export const Avatar = styled.div<{ $content: string }>`
+  height: 40vh;
+  object-fit: cover;
+  background: ${(props) => `url(${props.$content})`} no-repeat center;
+  background-size: cover;
+  border-radius: 6px;
+  border: 4px solid ${colors.primaryLighten};
+  ${MD} {
+    height: 500px;
+  }
+`
+export const DetailWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${MD} {
+    width: 50%;
+  }
+`
+export const SummaryWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  gap: 10px;
+  letter-spacing: 0.1em;
+`
+export const ShelterName = styled.h2`
+  font-weight: bold;
+  color: ${colors.gray2};
+  font-size: 18px;
+`
+export const LabelAndValue = styled.div`
+  display: flex;
+`
+export const Label = styled.label`
+  color: ${colors.gray5};
+  margin-right: 12px;
+  font-size: 15px;
+`
+export const Value = styled.span`
+  font-size: 15px;
+  color: ${colors.gray3};
+`
+export const UpdateTime = styled(Value)`
+  color: ${colors.gray5};
+`
+export const InfoBoxWrap = styled.div`
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  ${MD} {
+    /* margin-top:10px ; */
+    height: 10px;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    bottom: -90px;
+    gap: 20px;
+  }
+`
+export const IntroWrap = styled.div`
+  margin-top: 18px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
+export const IntroLabel = styled(Label)`
+  font-size: 16px;
+  color: ${colors.secondary};
+  font-weight: bold;
+`
+export const IntroValue = styled(Value)`
+  font-size: 15px;
+  color: ${colors.gray5};
+`
