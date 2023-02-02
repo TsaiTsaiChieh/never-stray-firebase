6747adca38eac3d466844d0080e643f7f7472cd6
Tsai-Chieh <d0177381@gmail.com>
2023-02-02 22:34:18 +0800

    :sparkles: add Website link

--- src/pages/Pet/Contact.tsx
+++ src/pages/Pet/Contact.tsx
@@ -5,19 +5,17 @@ import {
   Label,
   LabelAndValue,
   Value,
+  Website,
 } from '../../styles/pages/Pet'
+import { b64DecodeUnicode } from '../../utils/helper'
 
 interface Props {
   profile: PetType
 }
 const Contact = ({ profile }: Props) => {
-  const { animal_place, shelter_tel, shelter_address } = profile
+  const { shelter_tel, shelter_address, animal_subid } = profile
   return (
     <ContactWrap>
-      <LabelAndValue>
-        <Label>{i18n.t('labels.shelter')}</Label>
-        <Value>{animal_place}</Value>
-      </LabelAndValue>
       <LabelAndValue>
         <Label>{i18n.t('labels.phone')}</Label>
         <Value>{shelter_tel}</Value>
@@ -25,6 +23,16 @@ const Contact = ({ profile }: Props) => {
       <LabelAndValue>
         <Label>{i18n.t('labels.address')}</Label>
         <Value>{shelter_address}</Value>
+      </LabelAndValue>
+      <LabelAndValue>
+        <Label>{i18n.t('labels.link')}</Label>
+        <Website
+          href={`https://www.pet.gov.tw/AnimalApp/AnnounceSingle.aspx?PageType=Announce&AcNum=${b64DecodeUnicode(animal_subid)}`}
+          target='_blank'
+          rel='noreferrer'
+        >
+          {i18n.t('labels.website')}
+        </Website>
       </LabelAndValue>
     </ContactWrap>
   )
