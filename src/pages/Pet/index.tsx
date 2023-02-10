a033235de87b4d86d1b469129831e7499f1ef806
Tsai-Chieh <d0177381@gmail.com>
2023-02-10 19:19:37 +0800

    :sparkles: profile load from petSlice or api

--- src/pages/Pet/index.tsx
+++ src/pages/Pet/index.tsx
@@ -5,9 +5,11 @@ import { useLocation, useParams } from 'react-router-dom'
 import Profile from './Profile'
 import { Footer, Header } from '../../components'
 import { useGetPetMutation } from '../../services/api'
+import { useAppSelector } from '../../store/hook'
 
 const Pet = () => {
-  const [profile, setProfile] = useState<PetType | undefined>(undefined)
+  const { pets, currIdx } = useAppSelector((state) => state.pet)
+  const [profile, setProfile] = useState<PetType | undefined>(pets.length !== 0 && typeof (currIdx) === 'number' ? pets[currIdx] : undefined)
   const location = useLocation()
   const { id } = useParams()
   const [getPet] = useGetPetMutation()
@@ -17,11 +19,11 @@ const Pet = () => {
       const result = await getPet({ id: id! }).unwrap()
       setProfile(result[0])
     }
-    if (location.state !== null) setProfile(location.state.detail)
+    if (pets.length === 0 && location.state !== null) setProfile(location.state.detail)
     else {
       getPetDetail()
     }
-  }, [location.state, id, getPet])
+  }, [pets, location.state, id, getPet])
 
   return (
     <>
